7143b170c5597fc91ee287a0d1ebcca8ccdd6279
Vivek Chandra <vivekchandra007@gmail.com>
2025-04-30 21:59:12 +0530

    login page partial and MSG91 script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Image from "next/image";
-import "./globals.css";
+import "../styles/globals.css";
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,14 +33,14 @@ export default function RootLayout({
       >
         {children}
         <Image
-        className="absolute -z-10 top-0 left-0 w-full h-full"
-        src="/background.png"
-        alt="Devotees' Association background image"
-        sizes="100v"
-        objectFit="cover"
-        fill
-        priority
-      />
+          className="absolute -z-10 top-0 left-0 w-full h-full"
+          src="/background.png"
+          alt="Devotees' Association background image"
+          sizes="100v"
+          objectFit="cover"
+          fill
+          priority
+        />
       </body>
     </html>
   );
---
+++ src/app/login/page.tsx
@@ -0,0 +1,119 @@
+'use client';
+
+import { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import { Button } from "primereact/button";
+import { Card } from 'primereact/card';
+import { Messages } from 'primereact/messages';
+import { Divider } from "primereact/divider";
+
+
+export default function LoginPage() {
+    const [authInProgress, setAuthInProgress] = useState<boolean>(false);
+    const msgs: any = useRef(null);
+
+    const header = (
+        <Image
+            src="/app-image.jpg"
+            alt="Devotees' Association"
+            width={500}
+            height={500}
+            priority
+        />
+    );
+
+    const title = (
+        <span className="text-3xl font-light text-indigo-900">
+            <span className="text-orange-400">Dev</span>otees'&nbsp; 
+            <span className="text-orange-400">Association</span>
+            <Divider align="center">
+                <a className="hover:underline"href="https://vedabase.io/en/library/cc/adi/3/91/" target="_blank" rel="noopener noreferrer">
+                    <span className="p-tag p-tag-warning">विष्णु-भक्तः स्मृतो दैव</span>
+                </a>
+            </Divider>
+        </span>
+    );
+
+    const subTitle = (
+        <span className="text-sm text-gray-700">
+            As mentioned in
+            &nbsp;
+            <a className="hover:underline text-indigo-950"href="https://vedabase.io/en/library/cc/adi/3/91/" target="_blank" rel="noopener noreferrer">
+                Śrī Caitanya-Caritāmṛta Ādi 3.91 &nbsp;<i className="pi pi-external-link"></i>
+            </a>
+            <br />
+            The devotees of Lord Viṣṇu are godly <span className="text-orange-400">(dev)</span>.
+            <br />and we know Association of Devotees is one of the most important activity in Bhakti.
+            <br />
+            So, let us connect with you.
+        </span>
+    );
+    const footer = (
+        <div>
+            <small>
+                <Divider align="center" color="orange">
+                </Divider>
+                <span className="flex justify-content-space-between">
+                    <a href="https://iskconpunebcec.com/#/Home" target="_blank" rel="noopener noreferrer"className="hover:underline">
+                        ISKCON BCEC
+                    </a>
+                    <a href="https://iskconpunebcec.com/#/newtemple" className="hover:underline" target="_blank" rel="noopener noreferrer">
+                        NEW TEMPLE
+                    </a>
+                </span>
+            </small>
+        </div>
+    );
+
+    const configuration = {
+        widgetId: process.env.NEXT_PUBLIC_MSG91_WIDGET_ID!,
+        tokenAuth: process.env.NEXT_PUBLIC_MSG91_AUTH_KEY!,
+        success: (data: any) => {
+            // get verified token in response
+            setAuthInProgress(false);
+            console.log('success response', data);
+        },
+        failure: (error: any) => {
+            setAuthInProgress(false);
+            console.log('failure reason', error);
+        },
+    };
+
+    function initSendOTP(configuration: { widgetId: string; tokenAuth: string; success: (data: any) => void; failure: (error: any) => void; }): void {
+        if (typeof window !== 'undefined' && (window as any).initSendOTP) {
+            const sendOTP = (window as any).initSendOTP;
+            setAuthInProgress(true);
+            sendOTP({
+                widgetId: configuration.widgetId,
+                tokenAuth: configuration.tokenAuth,
+                success: configuration.success,
+                failure: configuration.failure,
+            });
+        } else {
+            console.error('SendOTP script is not loaded or unavailable.');
+        }
+    }
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = process.env.NEXT_PUBLIC_MSG91_WIDGET_SCRIPT_URL!;
+        script.async = true;
+        document.body.appendChild(script);
+    }, []);
+
+    return (
+        <div className="grid items-center justify-items-center min-h-screen">
+            <div className="row-start-2">  
+                <Card title={title}
+                    subTitle={subTitle} footer={footer} header={header}
+                    className="shadow-2xl w-92 md:w-110 text-center card-transparent">
+                    <div>
+                        <Button id="btn-sign-in" label="Login with your Mobile Number" severity="danger" raised
+                            icon="pi pi-mobile" loading={authInProgress}
+                            onClick={() => initSendOTP(configuration)} />
+                    </div>
+                    <Messages ref={msgs} />
+                </Card>
+            </div>
+        </div>
+    );
+}
