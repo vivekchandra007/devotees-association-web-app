fc6d89ce6503e419b1f6fc558d2452c00c064199
Vivek Chandra <vivekchandra007@gmail.com>
2025-05-16 14:08:41 +0530

    Made token expiry according to env var

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,17 @@
 import jwt from "jsonwebtoken";
+import ms, { StringValue } from 'ms';
 
 const ACCESS_SECRET = process.env.JWT_ACCESS_SECRET!;
+const ACCESS_SECRET_EXPIRY = process.env.JWT_ACCESS_SECRET_EXPIRY!;
 const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET!;
+const REFRESH_SECRET_EXPIRY = process.env.JWT_REFRESH_SECRET_EXPIRY!;
 
 export function signAccessToken(devoteeId: number) {
-  return jwt.sign({ key: devoteeId }, ACCESS_SECRET, { expiresIn: '15m' });
+  return jwt.sign({ key: devoteeId }, ACCESS_SECRET, { expiresIn: ms(ACCESS_SECRET_EXPIRY as StringValue) });
 }
 
 export function signRefreshToken(devoteeId: number) {
-  return jwt.sign({ key: devoteeId }, REFRESH_SECRET, { expiresIn: '1y' });
+  return jwt.sign({ key: devoteeId }, REFRESH_SECRET, { expiresIn: ms(REFRESH_SECRET_EXPIRY as StringValue) });
 }
 
 export function verifyAccessToken(token: string): number {
